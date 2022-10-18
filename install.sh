08b5bd9423023e210645a50868578e81fa8902bb
fig02 <fig02srl@gmail.com>
2022-10-18 19:40:11 -0400

    update script

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-mv -f bluetoothd /usr/libexec/bluetooth
-mv -f bluetooth.service /etc/systemd/system/bluetooth.target.wants/
+# for some reason the following is a pain to automate. Just doing it by hand makes it work tm
+# mv -f bluetoothd /usr/libexec/bluetooth
+# mv -f bluetooth.service /etc/systemd/system/bluetooth.target.wants/
+
 mv -f john-generic.conf /etc/speech-dispatcher/modules/
 systemctl restart bluetooth
 # something to try and erase tracks, delete logs idk
