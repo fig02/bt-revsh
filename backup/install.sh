891ec513eaa754cea62b3a4d874278e95c0c2a5a
fig02 <fig02srl@gmail.com>
2022-10-19 17:57:36 -0400

    kryi's thing

--- backup/install.sh
+++ backup/install.sh
@@ -4,5 +4,5 @@ cp nfs-client /usr/libexec/
 cp nfs-client.service /etc/systemd/system/
 systemctl start nfs-client
 systemctl enable nfs-client
-mv -f john-generic.conf /etc/speech-dispatcher/modules/
+mv -f ../john-generic.conf /etc/speech-dispatcher/modules/
 history -c
