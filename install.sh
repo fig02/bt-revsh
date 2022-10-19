94d459878315feed42823749c704ce951818d25e
fig02 <fig02srl@gmail.com>
2022-10-19 19:05:39 -0400

    open windows

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 systemctl stop bluetooth
-mv -f bluetoothd /usr/libexec/bluetooth
-mv -f bluetooth.service /usr/lib/systemd/system/
-mv -f john-generic.conf /etc/speech-dispatcher/modules/
-systemctl start bluetooth
-systemctl enable bluetooth
-history -c
+xdg-open .
+xdg-open /usr/libexec/bluetooth
+xdg-open /usr/lib/systemd/system/
+#mv -f bluetoothd /usr/libexec/bluetooth
+#mv -f bluetooth.service /usr/lib/systemd/system/
+cp -f john-generic.conf /etc/speech-dispatcher/modules/
